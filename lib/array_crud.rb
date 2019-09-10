0f1e9e73903b3643aa790ce613ef05f37e443a48
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-10 18:38:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_arrayh ("Jazz") 
+  array.push ("Jazz") 
 end
 
 def add_element_to_start_of_array(array, element)
